7564103ef4632f65968218c4d0647458b84609ae
Jesse Ma <j.masonoma17@gmail.com>
2018-07-09 00:32:02 -0700

    Initial commit for toDoApp

---
+++ toDoApp/www/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="./styles.css">
+    <title> To-Do App: PHP </title>
+</head>
+<body>
+    <h1> <?php echo 'To Do List'; ?> </h1>
+</body>
+</html>
